3b8d6cf8beac2c614b9904c112861111698e0fd4
rawera_daniel <rawera.daniel@gmail.com>
2023-10-21 10:34:48 +0300

    feat: Set up pages, routes, and install styled components and React Router library

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,35 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+
+import GlobalStyle from "./styles/GlobalStyles";
+import Dashboard from "./pages/Dashboard";
+import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import Users from "./pages/Users";
+import Settings from "./pages/Settings";
+import Account from "./pages/Account";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
+
 function App() {
-  return <div>Hello!</div>;
+  return (
+    <>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Navigate replace to="dashboard" />}></Route>
+          <Route path="dashboard" element={<Dashboard />}></Route>
+          <Route path="bookings" element={<Bookings />}></Route>
+          <Route path="cabins" element={<Cabins />}></Route>
+          <Route path="users" element={<Users />}></Route>
+          <Route path="settings" element={<Settings />}></Route>
+          <Route path="account" element={<Account />}></Route>
+
+          <Route path="login" element={<Login />}></Route>
+          <Route path="*" element={<PageNotFound />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
