7dbbb800af20f9d44332552e04b07678eda502ea
rawera_daniel <rawera.daniel@gmail.com>
2023-11-10 12:04:12 +0300

    feat: add setting update function to update its data

---
+++ src/features/settings/useUpdateSetting.js
@@ -0,0 +1,22 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+import { updateSetting as updateSettingApi } from "../../services/apiSettings";
+
+export function useUpdateSetting() {
+  const queryClint = useQueryClient();
+
+  const { isLoading: isUpdating, mutate: updateSetting } = useMutation({
+    mutationFn: updateSettingApi,
+    onSuccess: () => {
+      toast.success("Setting successfully updated");
+
+      queryClint.invalidateQueries({
+        queryKey: ["settings"],
+      });
+    },
+
+    onError: (err) => toast.error(err.message),
+  });
+
+  return { isUpdating, updateSetting };
+}
