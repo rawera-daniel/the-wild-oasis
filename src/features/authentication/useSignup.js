b3c15b2d65bb0a7ca01b9272641427eab8f8d7b0
rawera_daniel <rawera.daniel@gmail.com>
2023-11-23 19:58:26 +0300

    feat:  enable account creation for new users

---
+++ src/features/authentication/useSignup.js
@@ -0,0 +1,16 @@
+import { useMutation } from "@tanstack/react-query";
+import { signup as signUpApi } from "../../services/apiAuth";
+import toast from "react-hot-toast";
+
+export function useSignup() {
+  const { mutate: signup, isLoading } = useMutation({
+    mutationFn: signUpApi,
+    onSuccess: () => {
+      toast.success(
+        "Account successfully created! Please verify the new account fron the user's email address"
+      );
+    },
+  });
+
+  return { signup, isLoading };
+}
